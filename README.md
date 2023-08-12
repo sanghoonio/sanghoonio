01cdd4cab699c2a989ce4d53378cba46edc24805
Sam Park <samp1800@live.com>
2023-08-12 17:02:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sanghoonio
+
+hello
---
+++ app.R
@@ -0,0 +1,17 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+source('ui.R', local = TRUE)
+source('server.R', local = TRUE)
+
+# Run the application 
+shinyApp(ui = ui, server = server)
