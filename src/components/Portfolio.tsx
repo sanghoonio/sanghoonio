3286c81ddb9a121f2cc4d0fad84dfb1015cc44c9
Sam Park <samp1800@live.com>
2024-08-05 01:10:17 -0400

    minor resume and portfolio update

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,8 +7,23 @@ const Portfolio: React.FC = () => {
     //   <a className='btn btn-outline-dark p-4 rounded-4' href='https://sanghoon.io/shiny/lifepalette/' target='_blank' rel='noopener noreferrer'>LifePalette</a>
     //   <a className='btn btn-outline-dark p-4 rounded-4' href='https://sanghoon.io/shiny/navisnp/' target='_blank' rel='noopener noreferrer'>NaviSNP</a>
     // </div>
-    <div>
-    <h4>Coming Soon!</h4>
+    <div className='text-start'>
+      {/*<h4>Coming Soon!</h4>*/}
+
+      <a className='card mb-3 d-block rounded-1 border-dark text-decoration-none' href='https://sanghoon.io/shiny/lifepalette/' target='_blank' rel='noopener noreferrer'>
+        <div className='card-body'>
+          <h5 className='card-title'>LifePalette</h5>
+          <p className='card-text'>a life visualization tool | built with R Shiny</p>
+        </div>
+      </a>
+
+      <a className='card mb-3 d-block rounded-1 border-dark text-decoration-none' href='https://sanghoon.io/shiny/navisnp/' target='_blank' rel='noopener noreferrer'>
+        <div className='card-body'>
+          <h5 className='card-title'>NaviSNP</h5>
+          <p className='card-text'>a global allele frequency viewer | built with R Shiny</p>
+        </div>
+      </a>
+
     </div>
   );
 };
