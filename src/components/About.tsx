4ed3ff99fd5ca57236bf159f644dd939f14a3f81
Sam Park <samp1800@live.com>
2024-09-20 18:31:47 +0000

    resume

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About: React.FC = () => {
       <h4 className='semiboldish'>Hello!</h4>
       <div>
         <p>I am an engineer with a B.S. in biomedical engineering and a minor in computer science from UVA. As a student, I was able to discover my interest in applying data science and computational modeling to biology and genetics through my courses and various projects. These experiences have contributed to my excitement for a new priority in our field: personalized, data-driven medicine.</p>
-        <p>Since graduating, I have worked with Merck as an automation engineer to support their Gardasil HPV vaccine production process and learn about the manufacturing side of the pharmaceutical industry while simultaneously pursuing my interests in genomics part-time with Predictiv Care Inc., whose mission is to provide a novel DNA-based digital twin service by incorporating our genetic data.</p>
+        <p>Since graduating, I have worked with Merck as an automation engineer to support their Gardasil HPV vaccine production process and learn about the manufacturing side of the pharmaceutical industry while simultaneously pursuing my interests in genomics part-time with Predictiv Care Inc., whose mission is to provide a novel DNA-based digital twin service by incorporating our genetic data. I am currently a data analyst in the lab of Dr. Nathan Sheffield to explore epigenetics and develop my skills in software development.</p>
       </div>
 
       <p className="d-inline-flex gap-2">
